fb748e9adde59d81cb47239e0381f5c033fe8ad0
igor.mihalik@gmail.com <igor.mihalik@gmail.com@7f545a62-fe31-11de-8589-c9554b7fe0d5>
2010-01-16 21:38:24 +0000

    changed numbering of phases to match real image order

--- trunk/carousel/src/main/java/sk/gtug/carousel/client/Carousel.java
+++ trunk/carousel/src/main/java/sk/gtug/carousel/client/Carousel.java
@@ -18,125 +18,139 @@ import com.google.gwt.user.client.ui.LayoutPanel;
 
 public class Carousel extends LayoutPanel {
 
-    private List<CarouselImagePanel> panels = new ArrayList<CarouselImagePanel>();
-    private boolean active = false;
-    private CarouselImageProvider imageProvider;
-    private int actualImageIndex;
+	private List<CarouselImagePanel> panels = new ArrayList<CarouselImagePanel>();
+	private boolean active = false;
+	private CarouselImageProvider imageProvider;
+	private int actualImageIndex;
 
-    public Carousel() {
-        panels.add(new CarouselImagePanel(this, 0));
-        panels.add(new CarouselImagePanel(this, 1));
-        panels.add(new CarouselImagePanel(this, 2));
-        panels.add(new CarouselImagePanel(this, 3));
-        panels.add(new CarouselImagePanel(this, 4));
-        panels.add(new CarouselImagePanel(this, 5));
+	public Carousel() {
+		panels.add(new CarouselImagePanel(this, 0));
+		panels.add(new CarouselImagePanel(this, -1));
+		panels.add(new CarouselImagePanel(this, -2));
+		panels.add(new CarouselImagePanel(this, -3));
+		panels.add(new CarouselImagePanel(this, 1));
+		panels.add(new CarouselImagePanel(this, 2));
 
-        for (CarouselImagePanel panel : panels)
-            add(panel);
+		for (CarouselImagePanel panel : panels)
+			add(panel);
 
-        addDomHandler(new ClickHandler() {
-            public void onClick(ClickEvent event) {
-                moveRectangle(1);
-            }
-        }, ClickEvent.getType());
-        addDomHandler(new MouseWheelHandler() {
-            public void onMouseWheel(MouseWheelEvent event) {
-                int deltaY = event.getDeltaY();
-                moveRectangle(deltaY);
-            }
-        }, MouseWheelEvent.getType());
+		addDomHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				moveRectangle(1);
+			}
+		}, ClickEvent.getType());
+		addDomHandler(new MouseWheelHandler() {
+			public void onMouseWheel(MouseWheelEvent event) {
+				int deltaY = event.getDeltaY();
+				moveRectangle(deltaY);
+			}
+		}, MouseWheelEvent.getType());
 
-    }
+	}
 
-    private void moveRectangle(int delta) {
-        if (imageProvider == null)
-            return;
-        if (active) {
-            return;
-        }
-        if (delta < 0 && actualImageIndex == 0)
-            return;
-        if (delta > 0 && actualImageIndex == this.imageProvider.size() - 1)
-            return;
-        if (delta < 0)
-            this.actualImageIndex--;
-        else
-            this.actualImageIndex++;
-        Map<CarouselImagePanel, CarouselImagePanel.AnimationInfo> panelAnimationInfo = new HashMap<CarouselImagePanel, CarouselImagePanel.AnimationInfo>();
-        int steps = 20;
-        for (CarouselImagePanel panel : panels) {
-            int lastPhase = panel.updatePhase(delta > 0 ? 1 : -1);
-            ImageRect rectForPhase = panel.getImageRectForCurrentPhase();
-            AnimationInfo animInfo = panel.getAnimationInfo(rectForPhase, steps, lastPhase);
-            panelAnimationInfo.put(panel, animInfo);
-            panel.updateStyle(panelAnimationInfo.get(panel));
-        }
-        animate(panelAnimationInfo, steps, delta);
-    }
+	private void moveRectangle(int delta) {
+		if (imageProvider == null)
+			return;
+		if (active) {
+			return;
+		}
+		if (delta < 0 && actualImageIndex == 0)
+			return;
+		if (delta > 0 && actualImageIndex == this.imageProvider.size() - 1)
+			return;
+		if (delta < 0)
+			this.actualImageIndex--;
+		else
+			this.actualImageIndex++;
+		Map<CarouselImagePanel, CarouselImagePanel.AnimationInfo> panelAnimationInfo = new HashMap<CarouselImagePanel, CarouselImagePanel.AnimationInfo>();
+		int steps = 20;
+		for (CarouselImagePanel panel : panels) {
+			int lastPhase = panel.updatePhase(delta > 0 ? 1 : -1);
+			ImageRect rectForPhase = panel.getImageRectForCurrentPhase();
+			AnimationInfo animInfo = panel.getAnimationInfo(rectForPhase,
+					steps, lastPhase);
+			panelAnimationInfo.put(panel, animInfo);
+			panel.updateStyle(panelAnimationInfo.get(panel));
+		}
+		animate(panelAnimationInfo, steps, delta);
+	}
 
-    private void animate(final Map<CarouselImagePanel, CarouselImagePanel.AnimationInfo> animInfo, final int steps, int delta) {
-        final Set<CarouselImagePanel> panels = animInfo.keySet();
-        this.active = true;
-        for (CarouselImagePanel panel : panels) {
-            if (animInfo.get(panel).lastPhase == 3)
-                panel.setImageHandle(delta > 0 ? getImageUrl(actualImageIndex + 2) : getImageUrl(actualImageIndex - 2));
-        }
-        new Timer() {
-            int duration = 150;
-            double step = 1;
+	private void animate(
+			final Map<CarouselImagePanel, CarouselImagePanel.AnimationInfo> animInfo,
+			final int steps, int delta) {
+		final Set<CarouselImagePanel> panels = animInfo.keySet();
+		this.active = true;
+		for (CarouselImagePanel panel : panels) {
+			if (animInfo.get(panel).lastPhase == -3)
+				panel
+						.setImageHandle(delta > 0 ? getImageUrl(actualImageIndex + 2)
+								: getImageUrl(actualImageIndex - 2));
+		}
+		new Timer() {
+			int duration = 150;
+			double step = 1;
 
-            {
-                schedule(1);
-            }
+			{
+				schedule(1);
+			}
 
-            @Override
-            public void run() {
-                if (step == steps) {
-                    for (CarouselImagePanel panel : panels) {
-                        AnimationInfo aniInfo = animInfo.get(panel);
-                        panel.updatePanel(panel.calculateStepRect(step, aniInfo.stepX, aniInfo.stepY, aniInfo.stepWidth, aniInfo.lastRect));
-                    }
-                    Carousel.this.active = false;
-                } else {
-                    for (CarouselImagePanel panel : panels) {
-                        AnimationInfo aniInfo = animInfo.get(panel);
-                        double a = (step / steps) * (Math.PI / 2);
-                        double myStep = Math.sin(a) * steps;
-                        panel.updatePanel(panel.calculateStepRect(myStep, aniInfo.stepX, aniInfo.stepY, aniInfo.stepWidth, aniInfo.lastRect));
-                        this.schedule(duration / aniInfo.steps);
-                    }
-                    step++;
-                };
-            };
-        };
-    }
+			@Override
+			public void run() {
+				if (step == steps) {
+					for (CarouselImagePanel panel : panels) {
+						AnimationInfo aniInfo = animInfo.get(panel);
+						panel.updatePanel(panel.calculateStepRect(step,
+								aniInfo.stepX, aniInfo.stepY,
+								aniInfo.stepWidth, aniInfo.lastRect));
+					}
+					Carousel.this.active = false;
+				} else {
+					for (CarouselImagePanel panel : panels) {
+						AnimationInfo aniInfo = animInfo.get(panel);
+						double a = (step / steps) * (Math.PI / 2);
+						double myStep = Math.sin(a) * steps;
+						panel.updatePanel(panel.calculateStepRect(myStep,
+								aniInfo.stepX, aniInfo.stepY,
+								aniInfo.stepWidth, aniInfo.lastRect));
+						this.schedule(duration / aniInfo.steps);
+					}
+					step++;
+				}
+				;
+			};
+		};
+	}
 
-    public void setImageProvider(CarouselImageProvider imageProvider) {
-        this.imageProvider = imageProvider;
-        this.actualImageIndex = 0;
-        updateImageInPanels();
-    }
+	public void setImageProvider(CarouselImageProvider imageProvider) {
+		this.imageProvider = imageProvider;
+		this.actualImageIndex = 0;
+		updateImageInPanels();
+	}
 
-    private void updateImageInPanels() {
-        for (CarouselImagePanel panel : panels) {
-            if (panel.getPhase() == 0)
-                panel.setImageHandle(getImageUrl(actualImageIndex));
-            if (panel.getPhase() == 1)
-                panel.setImageHandle(getImageUrl(actualImageIndex - 1));
-            if (panel.getPhase() == 2)
-                panel.setImageHandle(getImageUrl(actualImageIndex - 2));
-            if (panel.getPhase() == 3)
-                panel.setImageHandle(null);
-            if (panel.getPhase() == 4)
-                panel.setImageHandle(getImageUrl(actualImageIndex + 2));
-            if (panel.getPhase() == 5)
-                panel.setImageHandle(getImageUrl(actualImageIndex + 1));
-        }
-    }
+	private void updateImageInPanels() {
+		for (CarouselImagePanel panel : panels) {
 
-    private String getImageUrl(int actualImageIndex) {
-        if (actualImageIndex < 0 || actualImageIndex >= imageProvider.size())
-            return null;
-        return imageProvider.getImageUrl(actualImageIndex).getUrl();
-    };
+			panel.setImageHandle(getImageUrl(actualImageIndex
+					+ panel.getPhase()));
+			//        	
+			// if (panel.getPhase() == 0)
+			// panel.setImageHandle(getImageUrl(actualImageIndex));
+			// if (panel.getPhase() == 1)
+			// panel.setImageHandle(getImageUrl(actualImageIndex - 1));
+			// if (panel.getPhase() == 2)
+			// panel.setImageHandle(getImageUrl(actualImageIndex - 2));
+			// if (panel.getPhase() == 3)
+			// panel.setImageHandle(null);
+			// if (panel.getPhase() == 4)
+			// panel.setImageHandle(getImageUrl(actualImageIndex + 2));
+			// if (panel.getPhase() == 5)
+			// panel.setImageHandle(getImageUrl(actualImageIndex + 1));
+		}
+	}
+
+	private String getImageUrl(int actualImageIndex) {
+		if (actualImageIndex < 0 || actualImageIndex >= imageProvider.size())
+			return null;
+		return imageProvider.getImageUrl(actualImageIndex).getUrl();
+	};
 }
--- trunk/carousel/src/main/java/sk/gtug/carousel/client/CarouselImagePanel.java
+++ trunk/carousel/src/main/java/sk/gtug/carousel/client/CarouselImagePanel.java
@@ -9,172 +9,165 @@ import com.google.gwt.user.client.ui.RequiresResize;
 
 class CarouselImagePanel extends Composite implements RequiresResize {
 
-    private final LayoutPanel parent;
-    private int phase;
-    private int boardWidth;
-    private int boardHeight;
-    private int width;
-    private final Image image;
-    private ImageRect currentRect;
+	private final LayoutPanel parent;
+	private int phase;
+	private int boardWidth;
+	private int boardHeight;
+	private int width;
+	private final Image image;
+	private ImageRect currentRect;
 
-    public CarouselImagePanel(LayoutPanel parent, int phase) {
-        this.parent = parent;
-        this.image = new Image();
-        this.phase = phase;
-        initWidget(image);
-        setVisible(false);
-    }
+	public CarouselImagePanel(LayoutPanel parent, int phase) {
+		this.parent = parent;
+		this.image = new Image();
+		this.phase = phase;
+		initWidget(image);
+		setVisible(false);
+	}
 
-    public void setZIndex(int zIndex) {
-        setZIndexPrivate(getElement(), zIndex);
-    }
+	public void setZIndex(int zIndex) {
+		setZIndexPrivate(getElement(), zIndex);
+	}
 
-    private native void setZIndexPrivate(Element el, int zIndex) /*-{
-        el.style.zIndex = zIndex;
-    }-*/;
+	private native void setZIndexPrivate(Element el, int zIndex) /*-{
+		el.style.zIndex = zIndex;
+	}-*/;
 
-    private ImageRect getImageRectForPhase(int phase) {
-        int x = boardWidth / 2;
-        int y = boardHeight / 2;
-        int width2 = width;
-        if (phase == 0) {
-            // no transformation needed
-        } else if (phase == 1) {
-            width2 = (int) (width2 * 0.71);
-            x = x - 5 - (width2 / 2) - width / 2;
-        } else if (phase == 2) {
-            width2 = (int) (width2 * 0.47);
-            x = (int) (x - (width / 2) - (width * 0.71) - width2 * 0.3);
-        } else if (phase == 3) {
-            width2 = (int) (width2 * 0.2);
-        } else if (phase == 4) {
-            width2 = (int) (width2 * 0.47);
-            x = (int) (x + (width / 2) + (width * 0.71) + width2 * 0.3);
-        } else if (phase == 5) {
-            width2 = (int) (width2 * 0.71);
-            x = x + 5 + (width2 / 2) + width / 2;
-        }
-        return new ImageRect(x, y, width2, width2);
-    }
+	private ImageRect getImageRectForPhase(int phase) {
+		int x = boardWidth / 2;
+		int y = boardHeight / 2;
+		int width2 = width;
+		if (phase == 0) {
+			// no transformation needed
+		} else if (phase == -1 || phase == 1) {
+			width2 = (int) (width2 * 0.71);
+			x = x + phase * (5 + (width2 / 2) + width / 2);
+		} else if (phase == -2 || phase == 2) {
+			width2 = (int) (width2 * 0.47);
+			double d = (width / 2) + (width * 0.71) + width2 * 0.3;
+			x = (int) (x + d * (phase / 2));
+		} else if (phase == -3) {
+			width2 = (int) (width2 * 0.2);
+		}
+		return new ImageRect(x, y, width2, width2);
+	}
 
-    public AnimationInfo getAnimationInfo(final ImageRect rect, final int steps, int lastPhase) {
-        double stepX = ((double) (rect.x - currentRect.x)) / steps;
-        double stepY = (rect.y - currentRect.y) / steps;
-        double stepWidth = (rect.width - currentRect.width) / steps;
-        final AnimationInfo anStep = new AnimationInfo(stepX, stepY, stepWidth, currentRect, rect, steps, lastPhase, phase);
-        return anStep;
-    }
+	public AnimationInfo getAnimationInfo(final ImageRect rect,
+			final int steps, int lastPhase) {
+		double stepX = ((double) (rect.x - currentRect.x)) / steps;
+		double stepY = (rect.y - currentRect.y) / steps;
+		double stepWidth = (rect.width - currentRect.width) / steps;
+		final AnimationInfo anStep = new AnimationInfo(stepX, stepY, stepWidth,
+				currentRect, rect, steps, lastPhase, phase);
+		return anStep;
+	}
 
-    public ImageRect calculateStepRect(double step, final double stepX, final double stepY, final double stepWidth, ImageRect rect) {
-        int newX = (int) (rect.x + step * stepX);
-        int newY = (int) (rect.y + step * stepY);
-        int newWidth = (int) (rect.width + step * stepWidth);
-        return new ImageRect(newX, newY, newWidth, newWidth);
-    }
+	public ImageRect calculateStepRect(double step, final double stepX,
+			final double stepY, final double stepWidth, ImageRect rect) {
+		int newX = (int) (rect.x + step * stepX);
+		int newY = (int) (rect.y + step * stepY);
+		int newWidth = (int) (rect.width + step * stepWidth);
+		return new ImageRect(newX, newY, newWidth, newWidth);
+	}
 
-    public void updatePanel(ImageRect rect) {
-        int x = rect.x;
-        int y = rect.y;
-        int width = rect.width;
-        int height = rect.height;
-        this.currentRect = rect;
-        int posX = x - width / 2;
-        int posY = y - height / 2;
-        parent.setWidgetLeftWidth(this, posX, Unit.PX, width, Unit.PX);
-        parent.setWidgetTopHeight(this, posY, Unit.PX, height, Unit.PX);
-        image.setPixelSize(width, height);
-    }
+	public void updatePanel(ImageRect rect) {
+		int x = rect.x;
+		int y = rect.y;
+		int width = rect.width;
+		int height = rect.height;
+		this.currentRect = rect;
+		int posX = x - width / 2;
+		int posY = y - height / 2;
+		parent.setWidgetLeftWidth(this, posX, Unit.PX, width, Unit.PX);
+		parent.setWidgetTopHeight(this, posY, Unit.PX, height, Unit.PX);
+		image.setPixelSize(width, height);
+	}
 
-    public ImageRect getImageRectForCurrentPhase() {
-        return getImageRectForPhase(phase);
-    }
+	public ImageRect getImageRectForCurrentPhase() {
+		return getImageRectForPhase(phase);
+	}
 
-    public int updatePhase(int delta) {
-        int lastPhase = phase;
-        this.phase += delta;
-        if (phase == 6)
-            phase = 0;
-        if (phase == -1)
-            phase = 5;
-        return lastPhase;
-    }
+	public int updatePhase(int delta) {
+		int lastPhase = phase;
+		this.phase -= delta;
+		if (phase == 3)
+			phase = -3;
+		if (phase == -4)
+			phase = 2;
+		return lastPhase;
+	}
 
-    public void updateStyle(AnimationInfo animInfo) {
-        this.setZIndex(getZIndex(animInfo));
-    }
+	public void updateStyle(AnimationInfo animInfo) {
+		this.setZIndex(getZIndex(animInfo));
+	}
 
-    private int getZIndex(AnimationInfo animInfo) {
-        int newPhase = animInfo.newPhase;
-        if (newPhase == 0)
-            return 0;
-        if (newPhase == 1 || newPhase == 5)
-            return -1;
-        if (newPhase == 2 || newPhase == 4)
-            return -2;
-        return -3;
-    }
+	private int getZIndex(AnimationInfo animInfo) {
+		int newPhase = animInfo.newPhase;
+		return -Math.abs(newPhase);
+	}
 
-    public int getPhase() {
-        return phase;
-    }
+	public int getPhase() {
+		return phase;
+	}
 
-    public void setPhase(int phase) {
-        this.phase = phase;
-    }
+	public void setPhase(int phase) {
+		this.phase = phase;
+	}
 
-    public void setImageHandle(String imageHandle) {
-        if (imageHandle == null) {
-            this.setVisible(false);
-        } else {
-            setVisible(true);
-            // if (!imageHandle.equals(image.getUrl()))
-            this.image.setUrl(imageHandle);
-        }
-    }
+	public void setImageHandle(String imageHandle) {
+		if (imageHandle == null) {
+			this.setVisible(false);
+		} else {
+			setVisible(true);
+			// if (!imageHandle.equals(image.getUrl()))
+			this.image.setUrl(imageHandle);
+		}
+	}
 
-    public static class ImageRect {
-        int x;
-        int y;
-        int width;
-        int height;
+	public static class ImageRect {
+		int x;
+		int y;
+		int width;
+		int height;
 
-        public ImageRect(int x, int y, int width, int height) {
-            this.x = x;
-            this.y = y;
-            this.width = width;
-            this.height = height;
-        }
-    }
+		public ImageRect(int x, int y, int width, int height) {
+			this.x = x;
+			this.y = y;
+			this.width = width;
+			this.height = height;
+		}
+	}
 
-    public static class AnimationInfo {
-        double stepX;
-        double stepY;
-        double stepWidth;
-        ImageRect lastRect;
-        ImageRect newRect;
-        int steps;
-        int lastPhase;
-        int newPhase;
+	public static class AnimationInfo {
+		double stepX;
+		double stepY;
+		double stepWidth;
+		ImageRect lastRect;
+		ImageRect newRect;
+		int steps;
+		int lastPhase;
+		int newPhase;
 
-        public AnimationInfo(double stepX, double stepY, double stepWidth, ImageRect lastRect, ImageRect newRect, int steps, int lastPhase,
-                int newPhase) {
-            super();
-            this.stepX = stepX;
-            this.stepY = stepY;
-            this.stepWidth = stepWidth;
-            this.lastRect = lastRect;
-            this.newRect = newRect;
-            this.steps = steps;
-            this.lastPhase = lastPhase;
-            this.newPhase = newPhase;
-        }
+		public AnimationInfo(double stepX, double stepY, double stepWidth,
+				ImageRect lastRect, ImageRect newRect, int steps,
+				int lastPhase, int newPhase) {
+			super();
+			this.stepX = stepX;
+			this.stepY = stepY;
+			this.stepWidth = stepWidth;
+			this.lastRect = lastRect;
+			this.newRect = newRect;
+			this.steps = steps;
+			this.lastPhase = lastPhase;
+			this.newPhase = newPhase;
+		}
 
-    }
+	}
 
-    public void onResize() {
-        this.boardWidth = parent.getElement().getClientWidth();
-        this.boardHeight = parent.getElement().getClientHeight();
-        this.width = (int) (Math.min(boardHeight * 0.95, boardWidth * 0.3) - 30);
-        updatePanel(getImageRectForPhase(phase));
-    }
+	public void onResize() {
+		this.boardWidth = parent.getElement().getClientWidth();
+		this.boardHeight = parent.getElement().getClientHeight();
+		this.width = (int) (Math.min(boardHeight * 0.95, boardWidth * 0.3) - 30);
+		updatePanel(getImageRectForPhase(phase));
+	}
 }
